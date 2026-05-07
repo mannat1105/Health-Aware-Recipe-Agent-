41fa411888f71a8df0912875de65c9d09ff1f998
mannat1105 <harmannatkaur12main@gmail.com>
2026-05-07 15:35:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ import time
 # ============================================================
 #  API KEYS
 # ============================================================
-GROQ_API_KEY = "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"  
-USDA_API_KEY = "1YoJf8yl79ZPgMrod2lIiCW4pFKuL0JavvG9Wz6u"   
+GROQ_API_KEY = "Insert key here"  
+USDA_API_KEY = "Insert key here"   
 
 # ============================================================
 #  SHARED STATE : accumulates results across all steps
